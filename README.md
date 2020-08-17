cf4c46765fbbbd6ea00cfe21664f1eca468917f7
vlad_puris_dev <vlad.puris.dev@gmail.com>
2020-08-17 15:16:57 -0400

    First sample commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # openapi-springfox2-sample
-Open API SpringFox version 2 sample
+A sample project of implementing Open API using SpringFox version 2 for a Spring Boot 2 project.
+
+## Getting Started
+Build the project and start the application. In order to directly test "/accounts/{accountId}" endpoint can be used "0123456789" account identifier.
+**Swagger UI URL**: http://localhost:6001/swagger-ui.html
+
+## Tech Stack
+- Spring Boot 2.3.2.RELEASE
+- SpringFox 2.9.2
+
+
---
+++ src/main/java/com/vvp/sample/config/Swagger2Config.java
@@ -0,0 +1,42 @@
+package com.vvp.sample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    /**
+     * Get swagger docket resource.
+     * @return swagger docket resource
+     */
+    @Bean
+    public Docket documentation() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.vvp"))
+                .paths(PathSelectors.ant("/v1/**"))
+                .build()
+                .pathMapping("/")
+                .apiInfo(metadata());
+    }
+
+    /**
+     * Get api metadata.
+     * @return api metadata
+     */
+    private ApiInfo metadata() {
+        return new ApiInfoBuilder()
+                .title("Account API")
+                .description("Details regarding available endpoints")
+                .version("1.0")
+                .build();
+    }
+}
